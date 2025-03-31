11a839693da635b4ed79f0871adf60896290f257
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 11:32:55 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -19,39 +19,3 @@ yay -S --noconfirm hyprland hyprpaper hyprlock hypridle
 # Cấu hình SDDM
 sudo systemctl enable sddm
 sudo systemctl start sddm
-
-# Tạo thư mục config
-mkdir -p ~/.config/hypr
-
-# Thêm cấu hình mặc định cho Hyprland
-cat <<EOT > ~/.config/hypr/hyprland.conf
-# Cấu hình cơ bản
-monitor=,preferred,auto,auto
-general {
-  gaps_in=5
-gaps_out=10
-  border_size=2
-  col.active_border=0xff89b4fa
-  col.inactive_border=0xff6c7086
-}
-
-decoration {
-  rounding=10
-}
-
-animations {
-  enabled=1
-  bezier=overshot,0.13,0.99,0.29,1.1
-  animation=windows,1,7,overshot
-  animation=border,1,10,default
-  animation=fade,1,7,default
-  animation=workspaces,1,6,overshot
-}
-
-master {
-  new_is_master=true
-}
-EOT
-
-# Thông báo hoàn tất
-echo "Cài đặt hoàn tất! Khởi động lại hệ thống để áp dụng thay đổi."
