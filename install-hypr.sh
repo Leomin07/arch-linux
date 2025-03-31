d3112839c1ced05e72e1d4ad6bc7b1081d4262b5
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 10:25:08 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,12 +1,25 @@
-cd
+#!/bin/bash
+
+# Lấy username của người dùng hiện tại
+USER_NAME=$(whoami)
+
+# Cài đặt yay
+echo "Đang cài đặt yay..."
 sudo git clone https://aur.archlinux.org/yay.git
-sudo chown -R minhtd:minhtd yay/
-cd yay/
+sudo chown -R $USER_NAME:$USER_NAME yay
+cd yay
 makepkg -s
-yay -Suy
+cd ..
+rm -rf yay  # Xóa thư mục sau khi cài đặt yay xong
+
+# Cập nhật hệ thống với yay
+yay -Suy --noconfirm
+
+# Cài đặt Hyprland
+echo "Đang cài đặt Hyprland..."
 cd /opt
 sudo git clone https://github.com/SolDoesTech/hyprland.git
-sudo chown -R minhtd:minhtd hyprland/
-cd hyprland/
+sudo chown -R $USER_NAME:$USER_NAME hyprland
+cd hyprland
 chmod +x set-hypr
 ./set-hypr
