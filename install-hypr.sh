7c17586491ea1a9a8ec84ad6fdff08ef48986215
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 10:51:34 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Cập nhật hệ thống
-sudo pacman -Syu --noconfirm
+sudo pacman -Syu --noconfirm -y
 
 # Cài đặt các gói cần thiết
-sudo pacman -S --noconfirm git base-devel wayland wayland-protocols wlroots xdg-desktop-portal-hyprland 
-sudo pacman -S --noconfirm hyprland waybar kitty rofi dunst brightnessctl pavucontrol network-manager-applet 
-sudo pacman -S --noconfirm grim slurp wl-clipboard xclip udisk2 sddm
+sudo pacman -S --noconfirm -y git base-devel wayland wayland-protocols wlroots xdg-desktop-portal-hyprland 
+sudo pacman -S --noconfirm -y hyprland waybar kitty rofi dunst brightnessctl pavucontrol network-manager-applet 
+sudo pacman -S --noconfirm -y grim slurp wl-clipboard xclip udisk2 sddm
 
 # Cài đặt yay nếu chưa có
 if ! command -v yay &> /dev/null; then
     git clone https://aur.archlinux.org/yay.git
     sudo chown -R $(whoami):$(whoami) yay
     cd yay
-    makepkg -si --noconfirm
+    makepkg -si --noconfirm -y
     cd ..
     rm -rf yay
 fi
 
 # Cài đặt các gói AUR
-yay -S --noconfirm hyprland-git hyprpaper-git hyprcursor-git grimblast
+yay -S --noconfirm -y hyprland-git hyprpaper-git hyprcursor-git grimblast
 
 # Kích hoạt SDDM
 sudo systemctl enable sddm
