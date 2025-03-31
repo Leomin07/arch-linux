fe3642a6f31ddf5ffc0d41ec80b7cf109e963970
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 10:20:57 +0700

    Update install-hyperv2.sh

--- install-hyperv2.sh
+++ install-hyperv2.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+# Clone repository từ GitHub
 git clone https://github.com/SolDoesTech/HyprV2.git
+
+# Di chuyển vào thư mục HyprV2
 cd HyprV2
+
+# Cấp quyền thực thi cho script set-hypr
 chmod +x set-hypr
+
+# Chạy script set-hypr
 ./set-hypr
