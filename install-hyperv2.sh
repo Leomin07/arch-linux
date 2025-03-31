76dd5fee3bf20e7d04a219b960c628a194a5e733
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 10:23:15 +0700

    Update install-hyperv2.sh

--- install-hyperv2.sh
+++ install-hyperv2.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# Clone repository từ GitHub
+echo "Cập nhật hệ thống và cài đặt yay..."
+# Cập nhật hệ thống
+sudo pacman -Syu --noconfirm
+
+# Cài đặt base-devel và git nếu chưa có
+sudo pacman -S --needed base-devel git --noconfirm
+
+# Clone và cài đặt yay nếu chưa có
+if ! command -v yay &> /dev/null; then
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay-bin
+else
+    echo "yay đã được cài đặt!"
+fi
+
+# Kiểm tra phiên bản yay
+yay --version
+
+echo "Cài đặt HyprV2..."
+# Clone repository HyprV2
 git clone https://github.com/SolDoesTech/HyprV2.git
 
 # Di chuyển vào thư mục HyprV2
