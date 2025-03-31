6fd7b2c84a9052c26c690ec78d2e3433b575eb17
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-03-31 09:48:29 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,7 +1,8 @@
+cd
 sudo git clone https://aur.archlinux.org/yay.git
 sudo chown -R minhtd:minhtd yay-git/
 cd yay/
-makepkg -si
+makepkg -s
 yay -Suy
 cd /opt
 sudo git clone https://github.com/SolDoesTech/hyprland.git
