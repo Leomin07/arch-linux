5b5f9d24730b80c8156be0cc291ed34815377735
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-04-01 23:49:30 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,41 +1,6 @@
 #!/bin/bash
 
-# Cập nhật hệ thống
-echo "Updating system..."
-sudo pacman -Syu --noconfirm
-
-# Cài đặt các gói cần thiết
-echo "Installing required packages..."
-sudo pacman -S --needed --noconfirm git base-devel
-
-# Cài đặt yay (nếu chưa có)
-if ! command -v yay &> /dev/null; then
-    echo "Installing yay..."
-    git clone https://aur.archlinux.org/yay.git
-    cd yay && makepkg -si --noconfirm && cd .. && rm -rf yay
-fi
-
-# Cài đặt Hyprland và các tiện ích
-echo "Installing Hyprland and dependencies..."
-yay -S --noconfirm hyprland rofi waybar wofi dunst kitty thunar pavucontrol \
-     polkit-gnome grim slurp swappy mako alacritty starship \
-     brightnessctl nwg-look ttf-jetbrains-mono-nerd
-
-# Clone repo Hyprdots
-echo "Cloning Hyprdots repository..."
-git clone --recurse-submodules https://github.com/prasanthrangan/hyprdots.git ~/.config/hyprdots
-
-# Chạy script cài đặt
-echo "Running Hyprdots install script..."
-cd ~/.config/hyprdots && ./install.sh
-
-# Hoàn tất
-echo "Hyprdots installation complete! Restart your session and select Hyprland."
-
-# Clone repo Arch-Hyprland
-echo "Cloning Arch-Hyprland repository..."
-git clone https://github.com/prasanthrangan/hyprdots.git  ~/hyprdots
-
-# Chạy script cài đặt của Arch-Hyprland
-echo "Running Arch-Hyprland install script..."
-cd ~/hyprdots/scripts && chmod +x install.sh && ./install.sh
+git clone https://github.com/b3nj5m1n/dotfiles
+cd dotfiles/other/arch-meta/
+makepkg -s
+source PKGBUILD && yay -Syu --needed
