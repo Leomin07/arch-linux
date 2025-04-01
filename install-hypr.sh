cbf742aca5598e4c032c2c08a40178edaab8fbf8
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-04-02 00:00:27 +0700

    Update install-hypr.sh

--- install-hypr.sh
+++ install-hypr.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+sudo pacman -Syu
+sudo pacman -S xmonad xmonad-contrib ghc
+mkdir -p ~/.xmonad
+sudo pacman -S xmobar
 
-git clone https://github.com/b3nj5m1n/dotfiles
-cd dotfiles/other/arch-meta/
-makepkg -s
-source PKGBUILD && yay -Syu --needed
