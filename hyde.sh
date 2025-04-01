8e52f57c60fb9c1a12618a8332b103af0a44b4b6
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-04-01 13:26:30 +0700

    Update hyde.sh

--- hyde.sh
+++ hyde.sh
@@ -1,28 +1,4 @@
-#!/bin/bash
-
-# Cập nhật hệ thống và cài đặt yay (nếu chưa có)
-sudo pacman -Syu --noconfirm
-sudo pacman -S --needed --noconfirm git base-devel
-
-# Cài đặt yay từ AUR
-git clone https://aur.archlinux.org/yay.git ~/yay
-cd ~/yay
-makepkg -si --noconfirm
-
-# Cài đặt Hyprland từ AUR
-yay -S hyprland
-
-# Cài đặt các gói phụ trợ
-sudo pacman -S firefox xorg-xwayland wayland wlroots hyprpaper hyprlock hypridle kitty rofi waybar mako polkit-kde-agent network-manager-applet pipewire wireplumber brightnessctl alsa-utils pavucontrol swaybg swaylock grim slurp
-
-# Tạo thư mục cấu hình và sao chép cấu hình mặc định
-mkdir -p ~/.config/hypr
-cp /usr/share/hyprland/hyprland.conf ~/.config/hypr/
-
-# Thêm Hyprland vào XDG_SESSION (nếu chưa có)
-if ! grep -q 'exec Hyprland' ~/.xinitrc; then
-  echo 'exec Hyprland' >> ~/.xinitrc
-fi
-
-# Khởi động Hyprland
-Hyprland
+git clone -b 24.04 --depth=1  https://github.com/JaKooLit/Ubuntu-Hyprland.git ~/Ubuntu-Hyprland-24.04
+cd ~/Ubuntu-Hyprland-24.04
+chmod +x install.sh
+./install.sh
