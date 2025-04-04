e1f64f5aace62187badc57d713ea7bf39ad3f129
MinhTD <61413434+Leomin07@users.noreply.github.com>
2025-04-04 15:24:04 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,2 @@
-yay -S latte-dock-git wpgtk-git qt5-styleplugins python-pywal
-git clone https://github.com/ComplexPlatform/KDE-dotfiles
-cd KDE-dotfiles/ && cp -r {.local,.config,.themes,.ncmpcpp} ~/
+yay -S ml4w-hyprland
+ml4w-hyprland-setup
